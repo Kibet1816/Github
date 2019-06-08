14570b42c331ead0d3405d0b161f6cd04417c171
Denis Kibet <kibet1816@gmail.com>
2019-06-08 15:37:07 +0300

    Imported router module in routing module

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Route } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   declarations: []
 })
 export class RoutingModule { }
