04b2c547826860df25278ef2e14cf590d59b9bba
Denis Kibet <kibet1816@gmail.com>
2019-06-08 15:41:32 +0300

    Declare a routes array

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -2,6 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 
+import { SearchComponent } from '../search/search.component';
+import { SearchDisplayComponent } from '../search-display/search-display.component';
+
+const routes: Route[] = [
+  {path: '/search', component: SearchComponent},
+  {path: '/view', component: SearchDisplayComponent},
+  {path: '""', redirectTo: '/search', pathMatch: 'full'}
+];
+
 @NgModule({
   imports: [
     CommonModule,
