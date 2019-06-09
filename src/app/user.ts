f743910d09faf12b00bd360c889a0aa6fc0185e5
Denis Kibet <kibet1816@gmail.com>
2019-06-09 11:37:19 +0300

    Create instances for user model class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,9 @@
 export class User {
+  public name: string;
+  public company: string;
+  public email: string;
+  public repos: number;
+  public followers: number;
+  public following: number;
+
 }
